adaced51d800a9b1c105f401554d31252ddf820d
jeona <Naba3bd3636@gmail.com>
2024-12-15 01:23:19 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -118,6 +118,15 @@ document.querySelector('.js-products-grid')
          });
        }
 
+       let cartQuantity = 0;
+
+       cart.forEach((item) => {
+        cartQuantity += item.quantity;
+       });
+
+       document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+        
        console.log(cart);
       });
     });
