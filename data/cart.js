c7bb245a45c20ab4a8d237dca4db0352f5bf6d57
jeona <Naba3bd3636@gmail.com>
2025-02-18 22:52:00 +0300

    delete product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
- export const cart = [{
+ export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
  },{
@@ -24,4 +24,17 @@
      quantity: 1
     });
   }
+};
+
+//here we will create a function to remove a product from the cart first (create a new array) second (loop through the cart) third (add each product to the new array, exept for this product) which means the one that we are clicking delete for. then replace the cart with the new cart.
+export function removeFromCart(productId){
+  const newCart = [];
+
+  cart.forEach((cartItem) =>{
+    if (cartItem.productId !== productId){
+      newCart.push(cartItem)
+    }
+  })
+
+  cart = newCart;
 };
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let cartSummaryHTML = '';
@@ -39,7 +39,7 @@ cartSummaryHTML+=
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link js-delete-link link-primary" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -96,5 +96,13 @@ cartSummaryHTML+=
 
 document.querySelector('.js-order-summary').
  innerHTML = cartSummaryHTML;
+
+ document.querySelectorAll('.js-delete-link').
+ forEach((link) =>{
+ link.addEventListener('click', () => {
+  const productId =link.dataset.productId;
+  removeFromCart(productId);
+ })
+ })
 
  
