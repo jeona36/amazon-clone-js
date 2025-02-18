642f60c2c55c31862bb065a5cf4e83e7ff5ad846
jeona <Naba3bd3636@gmail.com>
2025-02-18 23:27:24 +0300

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
- export let cart = [{
+ export let cart = 
+ JSON.parse(localStorage.getItem('cart'));
+
+ if(!cart){
+ cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
  },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
  }];
+};
+
+ function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+ };
 
 
  export function addToCart(productId){
@@ -24,6 +33,8 @@
      quantity: 1
     });
   }
+
+  saveToStorage();
 };
 
 //here we will create a function to remove a product from the cart first (create a new array) second (loop through the cart) third (add each product to the new array, exept for this product) which means the one that we are clicking delete for. then replace the cart with the new cart.
@@ -37,4 +48,6 @@ export function removeFromCart(productId){
   })
 
   cart = newCart;
+
+  saveToStorage();
 };
